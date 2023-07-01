ce2215f96f0484d6d0171eb7f6474c3f0909a971
Ronald Kelechi <ronaldkelechi11@gmail.com>
2023-07-01 20:00:22 +0100

    Slightly done with SignUp UI

--- app/src/main/java/com/wehub/wexchange/SignUp.java
+++ app/src/main/java/com/wehub/wexchange/SignUp.java
@@ -3,26 +3,40 @@ package com.wehub.wexchange;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.telephony.PhoneNumberFormattingTextWatcher;
 import android.view.View;
 import android.widget.EditText;
 
+import java.util.HashMap;
+
 public class SignUp extends AppCompatActivity {
 
-    EditText nameEditText, telephoneEditText, passwordEditText, addressEditText;
+    EditText nameEditText, telephoneEditText, passwordEditText, addressEditText, aboutEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
+        telephoneEditText.addTextChangedListener(new PhoneNumberFormattingTextWatcher("NG"));
+
     }
 
     public void signup(View view) {
+        //Initializing EditTexts
         nameEditText = findViewById(R.id.signupEdittextName);
         telephoneEditText = findViewById(R.id.signupEdittextTelephone);
         passwordEditText = findViewById(R.id.signupEdittextPassword);
         addressEditText = findViewById(R.id.signupEdittextAddress);
+        aboutEditText = findViewById(R.id.signupEdittextAbout);
 
+        //Creating String from the edittext
+        String name = nameEditText.getText().toString().trim();
+        String telephone = telephoneEditText.getText().toString();
+        String password = passwordEditText.getText().toString().trim();
+        String address = addressEditText.getText().toString();
+        String about = aboutEditText.getText().toString();
 
+        HashMap<String, String> map = new HashMap<>();
     }
 }
--- app/src/main/java/com/wehub/wexchange/models/Ads.java
+++ app/src/main/java/com/wehub/wexchange/models/Ads.java
@@ -1,4 +1,59 @@
 package com.wehub.wexchange.models;
 
-public class Ads {
+import java.io.Serializable;
+
+public class Ads  implements Serializable {
+    String image;
+    String title;
+    String price;
+    String description;
+    String ownerName;
+
+    public Ads(String image, String title, String price, String description, String ownerName) {
+        this.image = image;
+        this.title = title;
+        this.price = price;
+        this.description = description;
+        this.ownerName = ownerName;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
 }
